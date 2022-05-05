d0cb71e2ae9edebd19fe3375736873ec4b595468
Onda-dev <manuelaltieri3@gmail.com>
2022-05-05 15:18:55 +0200

    Consegna corretta

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,8 +1,5 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
-// Crea due tag div con due id diversi:
-// un div avrà il testo colorato di rosso mentre l’altro di verde.
-// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
 // Fai inserire un numero, che chiameremo N, all’utente.
 const N = Number(prompt("Inserisci un numero"))
