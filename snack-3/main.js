161aa3e9205d3f0fbe30b082b6b21da408cf1a7e
Onda-dev <manuelaltieri3@gmail.com>
2022-05-05 21:16:21 +0200

    Snack-3 ok

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,11 +1,11 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
-const red = document.querySelector(".red")
-const green = document.querySelector(".green")
+const red = document.querySelector("#red")
+const green = document.querySelector("#green")
 
-// red.style.color = "red"
-// green.style.color = "green"
+red.style.color = "red"
+green.style.color = "green"
 
 const numbers = []
 for( let i = 0; i < 20; i++) {
