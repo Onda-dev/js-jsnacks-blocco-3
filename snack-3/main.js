419e686b62d5d0e68a8bbe1e4fb6bef7441bae4b
Onda-dev <manuelaltieri3@gmail.com>
2022-05-05 16:12:23 +0200

    Stampa ok, colori non funzionanti (?)

--- snack-3/main.js
+++ snack-3/main.js
@@ -4,16 +4,32 @@
 const red = document.querySelector(".red")
 const green = document.querySelector(".green")
 
-red.style.color = "red";
-green.style.color = "green";
+// red.style.color = "red"
+// green.style.color = "green"
 
 const numbers = []
 for( let i = 0; i < 20; i++) {
     numbers.push(Math.floor(Math.random() * 100) +1)
 }
 
+console.log(numbers)
 
-
-if (numbers.length % 2 === 0) {
-    console.log(numbers)
+const oddNumbers = []
+for (let i = 0; i < numbers.length; i++) {
+    if(i % 2 === 0) {
+        oddNumbers.push(numbers[i]);
+    }
 }
+
+const evenNumbers = []
+for (let i = 0; i < numbers.length; i++) {
+    if(i % 2 === 1) {
+        evenNumbers.push(numbers[i]);
+    }
+}
+
+document.getElementById("red").innerHTML = oddNumbers
+document.getElementById("green").innerHTML = evenNumbers
+
+console.log(oddNumbers)
+console.log(evenNumbers)
