c300cb1498d38b04957f5350ef0fd0768b9f188f
Onda-dev <manuelaltieri3@gmail.com>
2022-05-05 15:00:06 +0200

    Prova numeri casuali diversi in ogni array

--- snack-2/main.js
+++ snack-2/main.js
@@ -7,15 +7,21 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 const N = Number(prompt("Inserisci un numero"))
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
+// Creo array contenitore di array multipli
 const dadArray = []
+// Creo array con numeri
 const sonArray = []
 
-for( let i = 0; i < 10; i++) {
-    sonArray.push(Math.floor(Math.random() * 100) +1)
+// Creo 10 numeri casuali tra 1 e 100 e li pusho in arraySon
+while(sonArray.length < N) {
+    for( let i = 0; i < 10; i++) {
+        sonArray.push(Math.floor(Math.random() * 100) +1)
+    }
+    for(let i = 0; i < N; i++) {
+        dadArray.push(sonArray);
+    }  
 }
-
-for(let i = 0; i < N; i++) {
-    dadArray.push(sonArray)[i];
-}
+// Creo un sonArray in dadArray per il numero di volte indicato nel promtp
 
 console.log(dadArray)
